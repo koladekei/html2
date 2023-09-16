020ad7299358e4e40b99f030444c94bc0262d6fa
koladekei <kolade.ojo20@gmail.com>
2023-09-16 08:59:45 +0100

    new assignment console

---
+++ index.js
@@ -0,0 +1,68 @@
+// alert("hello world")
+// document.write("hello septa peeps")
+// console.log("am learning js")
+
+// console.log (1+1)
+// console.log("hey")
+
+// statement java script
+
+// variable
+
+// ways to declare a vairable
+// var
+// let
+// const
+// var church ="rccg";
+
+// var name = "dayo";
+// // var name = "septa"
+// function
+// console.log(church)
+// const name = "toyota";
+// console.log(name)
+
+// you can stART a varaible with number ot with java function
+// data type
+
+// String
+
+// let  shape = "triangle";
+
+// Number
+let age = 20
+let newage =10
+
+console.log (age + newage)
+//  Boolean
+
+let isFemiAwake = true
+
+console.log(typeof age)
+
+const website = "google"
+const url ="https://www." + website + ".com"
+
+console.log(url)
+
+const oldName = " bayo"
+const oldAge = 15
+const capi = " am not capping"
+console.log("I Know" + oldName + " is " + oldAge + "years old" + capi)
+
+const firstname = " kolade"
+const habitat = " akure"
+const school = " imole university"
+console.log( "my name is" + firstname + " i live at" + habitat + " and schooled in" + school)
+
+console.log( " Assignment")
+const word = " what's the"
+const question1 = " var"
+const question2 = " let"
+const question3 = " const?"
+console.log( word + " Difference between" + question1 + " ," + question2 + " ," + " and" + question3)
+
+const kvar = " var is used to declear a vairable that can be reasigned"
+const klet = " let is similar but has a block scope"
+const kconst = " const is used to declear a vairable that cannot be reasined"
+console.log( " In Javascript" + kvar + " while" + klet + " and" + kconst)
